0199f0bccb2074f3811d89d3355a8b2e5bbb6a37
Julien Smith <juliencain1933@gmail.com>
2025-04-01 22:49:05 -0500

    addedshit

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ function Home() {
       }}
     >
       <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
+      <p>Click the button below to lofffgout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
         Sign Out
       </Button>
